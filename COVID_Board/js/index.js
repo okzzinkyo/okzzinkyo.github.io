6952e4210a851842fb1fa6c568bff68852c35e6b
ock-kyung2 <dhrwlsrud94@naver.com>
2021-12-13 18:36:51 +0900

    예방방법 페이지 정보 추가 및 움직임 적용

--- COVID_Board/js/index.js
+++ COVID_Board/js/index.js
@@ -1,5 +1,4 @@
 $(window).ready(function(){
-
   function control_hide() {
     $("#control_box").css({
       height:0
@@ -17,4 +16,38 @@ $(window).ready(function(){
       control_hide();
     }
   });
-});
+
+  // 예방 방법 page
+  // 관련 기관 연락처 초기 상태 : show
+  $(".list_item:last-child .arrow").css({
+    transform: "translateY(-50%) rotateZ(-180deg)"
+  })
+  
+  // 선택한 list show & hide
+  $("#prevention_list > li").each(function(index){
+    $(this).attr('data-index',index);
+  }).click(function(){
+    // 선택한 li indext 번호
+    var i = $(this).attr('data-index');
+    // 선택한 li의 보여줄 자식요소
+    var target = $(".list_item[data-index="+i+"] > :last-child");
+    // 선택한 li 화살표 요소
+    var target_arrow = $(".list_item[data-index="+i+"] .arrow");
+    
+    if(target.hasClass('show') ==false){
+      // show
+      target.slideDown();
+      target_arrow.css({
+        transform: "translateY(-50%) rotateZ(-180deg)"
+      })
+      target.addClass('show');
+    } else if( target.hasClass('show') ==true){
+      // hide
+      target.slideUp();
+      target.removeClass('show');
+      target_arrow.css({
+        transform: "translateY(-50%)"
+      })
+    }
+  })
+})
