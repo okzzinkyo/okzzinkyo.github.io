312a490b1f567043d8e2bc6858a798e34c066a41
ock-kyung2 <dhrwlsrud94@naver.com>
2021-12-14 02:23:50 +0900

    js 호출 순서 수정  및 api 연동 후 차트로 데이터를 바로 넘길수 있도록 코드 수정

--- COVID_Board/js/ajax.js
+++ COVID_Board/js/ajax.js
@@ -1,4 +1,3 @@
-$(window).ready(function(){
 
   // 전세계 코로나 정보 호출
   $.ajax({
@@ -61,75 +60,110 @@ $(window).ready(function(){
     }
 
     // 국가 설정 박스 option 목록 생성
-    for(let i=0; i<response.Countries.length; i++){
+    for(var i=0; i<response.Countries.length; i++){
       var data = response.Countries[i].Country
       $("#countries").append("<option value='"+data+"'>"+data+"</option>")
     }
   });
   
-  //최근 4일 전세계 COVID19 확진자수 localStroage에 저장 
-  TotalCases = [];
+  // 차트 datasets - 전세계 확진자
+  var chart_data1 = Chart_confirmed.data.datasets[0].data;
+  // 차트 datasets - 선택된 국가 확진자
+  var chart_data1_1 = Chart_confirmed.data.datasets[1].data;
+  // 차트 datasets - 선택된 국가 사망자
+  var chart_data2 = Chart_deaths.data.datasets[0].data;
+  // 차트 datasets - 선택된 국가 회복자
+  var chart_data3 = Chart_recovered.data.datasets[0].data;
+
+  //최근 4일 전세계 COVID19 확진자수 차트에 반영
   $.ajax({
     "url": "https://corona.lmao.ninja/v2/historical/all",
     "method": "GET",
     "timeout": 0,
   }).done(function (response) {
-  //확진자 데이터
+  // 날짜 별 확진자 수
    var Case = response.cases
   //  객체(날짜:확진자 수) 중 value(확진자 수)만
    var CaseArr = Object.values(Case);
-  //  최근 정보 4개만 TotalCases 배열에 담기
-   for(let i=1; i<5; i++){
+  //  최근 정보 4개만 chart_data1에 담기
+   for(var i=1; i<5; i++){
      var info_total = CaseArr[CaseArr.length-i];
-     console.log(info_total)
-     TotalCases.push(info_total);
+     chart_data1.push(info_total);
     }
-    //배열 시간 순으로 변경
-    TotalCases.reverse();
-    localStorage.setItem('total',JSON.stringify(TotalCases));
+    //데이터 시간 순으로 변경
+    chart_data1.reverse();
+    Chart_confirmed.update();
   });
 
+  //차트 라벨 - 확진자 변화 x축
+  var chart_labels1 = Chart_confirmed.data.labels
+  //차트 라벨 - 사망자 변화 x축
+  var chart_labels2 = Chart_deaths.data.labels
+  //차트 라벨 - 회복자 변화 x축
+  var chart_labels3 = Chart_recovered.data.labels
+
+  // 차트에 바뀐 데이터 반영 함수
+  function Update () {
+    Chart_confirmed.update();
+    Chart_deaths.update();
+    Chart_recovered.update();
+  }
+
+  // sessionStorage에 저장된 설정 국가
+  var option_value = sessionStorage.getItem("option")
+
   // 그래프 초기 값 설정 - 국가 설정 유도
-  Selected= [];
+  if(option_value== null){
+    option_value = "south-africa"
+  }
+
+  // 설정된 국가에 따른 데이터 차트에 반영
   $.ajax({
-    "url": "https://api.covid19api.com/total/country/south-africa",
-    "method": "GET",
-    "timeout": 0,
-  }).done(function (response) {
-    for(let i=1; i<5; i++){
-      var info_default = response[response.length-i];
-      Selected.push(info_default);
-    }
-    localStorage.setItem('country',JSON.stringify(Selected));
-  }); 
-
-  //나라 선택시 최근 4일 COVID 정보 배열에 담기
-  $("#select").change(function(){
-    // selected value url 형식에 맞게 변환
-    var selected_value = $("#countries option:selected").text().replace(/ /gi,"-").toLowerCase();
-
-    Selected = [];
-    //API 필요 데이터 추출
-    $.ajax({
-      "url": "https://api.covid19api.com/total/country/"+selected_value,
+      "url": "https://api.covid19api.com/total/country/"+option_value,
       "method": "GET",
       "timeout": 0,
     }).done(function (response) {
-      console.log(response)
-      for(let i=1; i<5; i++){
-        var info = response[response.length-i];
-        Selected.push(info);
+      //최근 정보 4개만 chart_data1_1,2,3에 담기
+      for(var i=4; i>=1; i--){
+        // 설정된 국가의 확진자 데이터
+        var info_Confirmed = response[response.length-i].Confirmed;
+        // 설정된 국가의 사망자 데이터
+        var info_Deaths = response[response.length-i].Deaths;
+        // 설정된 국가의 회복자 데이터
+        var info_Recovered = response[response.length-i].Recovered;
+  
+        chart_data1_1.push(info_Confirmed);
+        chart_data2.push(info_Deaths)
+        chart_data3.push(info_Recovered)
+        Update();
       }
-      if(response ==null){
-        return false;
+  
+      // 최근 정보가 기록된 Date 차트 라벨에 반영 
+      for (var i=4; i>=1; i--){
+        var info_date = response[response.length-i].Date;
+        // month 영문 표기로 변환 배열
+        var monthNames = ["Jan", "Feb", "Mar", "Apr","May", "Jun","Jul", "Aug", "Sep", "Oct","Nov", "Dec"];
+        // String으로 되어있는 Date에서 월, 일 추출
+        var month = info_date.substring(5,7);
+        var day = info_date.substring(8,10);
+        // 원하는 형식으로 출력
+        var answers = day+"-"+monthNames[parseInt(month-1)]
+
+        //차트에 라벨 적용
+        chart_labels1.push(answers)
+        chart_labels2.push(answers)
+        chart_labels3.push(answers)
+        Update();
       }
+
+      // input 값 변경 시 선택값 출력
+      $("#select").change(function(){
+        // selected value url 형식에 맞게 변환
+        selected_value = $("#countries option:selected").text().replace(/ /gi,"-").toLowerCase();
+      })
+
+      // 선택 버튼 클릭시 sessionStorage에 저장
+      $("#btn_select").click(function(){
+        sessionStorage.setItem("option",selected_value);
+      });
     });
-  });
-
-  // 설정 btn click -> localStorage 데이터 저장
-  $("#btn_select").click(function(){
-    localStorage.setItem('country',JSON.stringify(Selected));
-  });
-
-  // 
-});
--- COVID_Board/js/chart.js
+++ COVID_Board/js/chart.js
@@ -1,50 +1,18 @@
-$(window).ready(function(){
-
-  //localStorage에 넣은 정보 추출
-  var total_data = JSON.parse(localStorage.getItem("total"));
-  var selected_data = JSON.parse(localStorage.getItem("country"));
-
-  if(selected_data == null){
-    alert("새로고침 해주세요.")
-    return false;
-  } else if (total_data == null){
-    alert("새로고침 해주세요.")
-    return false;
-  }
-
-  var selected_1=selected_data[3];
-  var selected_2=selected_data[2];
-  var selected_3=selected_data[1];
-  var selected_4=selected_data[0];
-
-  // 차트 x축 labels
-  date_arr = [];
-  for (let i=3; i>=0; i--){
-    function date_labels (x){
-      var monthNames = ["Jan", "Feb", "Mar", "Apr","May", "Jun","Jul", "Aug", "Sep", "Oct","Nov", "Dec"];
-      var date = selected_data[x].Date
-      var month = date.substring(5,7);
-      var day = date.substring(8,10);
-      return day+"-"+monthNames[parseInt(month-1)]
-    };
-    date_arr.push(date_labels(i));  
-  }
-
   //선택한 나라의 확진자 추이
   const ctx1 = document.getElementById("Chart_confirmed").getContext("2d");
-  Chart_confirmed = new Chart(ctx1, {
+  const Chart_confirmed = new Chart(ctx1, {
     type: "line",
     data: {
-      labels: date_arr,
+      labels: [],
       datasets: [
         { 
           label: "전세계",
-          data:total_data,
+          data:[],
           borderColor: "rgba(75, 192, 192, 1)",
         },
         {
           label: "설정한 지역",
-          data:[selected_1.Confirmed,selected_2.Confirmed,selected_3.Confirmed,selected_4.Confirmed],
+          data:[],
           borderColor: "rgba(255, 159, 64, 1)",
         },
       ],
@@ -64,14 +32,14 @@ $(window).ready(function(){
   
   //선택한 나라의 사망자 추이
   const ctx2 = document.getElementById("Chart_deaths").getContext("2d");
-  Chart_deaths = new Chart(ctx2, {
+  const Chart_deaths = new Chart(ctx2, {
     type: "line",
     data: {
-      labels: date_arr,
+      labels: [],
       datasets: [
         {
           label: "설정한 지역",
-          data:[selected_1.Deaths,selected_2.Deaths,selected_3.Deaths,selected_4.Deaths],
+          data:[],
           borderColor: "rgba(255, 159, 64, 1)",
         },
       ],
@@ -91,14 +59,14 @@ $(window).ready(function(){
 
   //선택한 나라의 회복자 추이
   const ctx = document.getElementById("Chart_recovered").getContext("2d");
-  Chart_recovered = new Chart(ctx, {
+  const Chart_recovered = new Chart(ctx, {
     type: "line",
     data: {
-      labels: date_arr,
+      labels: [],
       datasets: [
         {
           label: "설정한 지역",
-          data:[selected_1.Recovered,selected_2.Recovered,selected_3.Recovered,selected_4.Recovered],
+          data:[],
           borderColor: "rgba(255, 159, 64, 1)",
         },
       ],
@@ -115,4 +83,3 @@ $(window).ready(function(){
       }
     }
   });
-})
