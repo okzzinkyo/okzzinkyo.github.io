716b51dcf901ab3b42c132811ee0875fea85edc5
ock-kyung2 <dhrwlsrud94@naver.com>
2021-12-20 10:15:41 +0900

    제출 버튼 삭제, 차트 생성관련 코드 함수 선언으로 정리

--- COVID_Board/js/chart.js
+++ COVID_Board/js/chart.js
@@ -1,19 +1,24 @@
-  //선택한 나라의 확진자 추이
-  const ctx1 = document.getElementById("Chart_confirmed").getContext("2d");
-  const Chart_confirmed = new Chart(ctx1, {
+// Const로 정의할거면 엘리먼트 지우고 새로 만들어도 const로 정의가 되어있는게 있어서 이전 element에서 변한게 없음
+// chart.js 도 document ready에 들어가야하는게 정상이지만 script src가 html 상으로 제일 마지막에 있어서 안넣었음.
+
+  
+  var ctx1 = document.getElementById("Chart_confirmed").getContext("2d");
+  Chart_confirmed = new Chart(ctx1, {
     type: "line",
     data: {
       labels: [],
       datasets: [
-        { 
+        {
           label: "전세계",
-          data:[],
+          data: [],
           borderColor: "rgba(75, 192, 192, 1)",
+          yAxisID: 'y',
         },
         {
           label: "설정한 지역",
-          data:[],
+          data: [],
           borderColor: "rgba(255, 159, 64, 1)",
+          yAxisID: 'y1',
         },
       ],
     },
@@ -22,24 +27,40 @@
         legend: {
           display: true,
           position: "bottom",
-          labels:{
+          labels: {
             boxHeight: 1
           },
         }
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
       }
     }
   });
-  
-  //선택한 나라의 사망자 추이
+
   const ctx2 = document.getElementById("Chart_deaths").getContext("2d");
-  const Chart_deaths = new Chart(ctx2, {
+  Chart_deaths = new Chart(ctx2, {
     type: "line",
     data: {
       labels: [],
       datasets: [
         {
           label: "설정한 지역",
-          data:[],
+          data: [],
           borderColor: "rgba(255, 159, 64, 1)",
         },
       ],
@@ -49,7 +70,7 @@
         legend: {
           display: true,
           position: "bottom",
-          labels:{
+          labels: {
             boxHeight: 1,
           },
         }
@@ -58,15 +79,15 @@
   });
 
   //선택한 나라의 회복자 추이
-  const ctx = document.getElementById("Chart_recovered").getContext("2d");
-  const Chart_recovered = new Chart(ctx, {
+  var ctx3 = document.getElementById("Chart_recovered").getContext("2d");
+  Chart_recovered = new Chart(ctx3, {
     type: "line",
     data: {
       labels: [],
       datasets: [
         {
           label: "설정한 지역",
-          data:[],
+          data: [],
           borderColor: "rgba(255, 159, 64, 1)",
         },
       ],
@@ -76,7 +97,7 @@
         legend: {
           display: true,
           position: "bottom",
-          labels:{
+          labels: {
             boxHeight: 1
           },
         }
--- COVID_Board/js/index.js
+++ COVID_Board/js/index.js
@@ -1,60 +1,66 @@
-$(window).ready(function(){
+// 컨트롤 박스 및 배경 Show
+function control_hide() {
+  $('#control_box')
+    .css({
+      height: 0,
+    })
+    .removeClass('show');
+  $('#shadow').hide();
+}
+// 컨트롤 박스 및 배경 hide
+function control_show() {
+  $('#control_box')
+    .css({
+      height: 150,
+    })
+    .addClass('show');
+  $('#shadow').show();
+}
 
-  // 컨트롤 박스 및 배경 Show 
-  function control_hide() {
-    $("#control_box").css({
-      height:0
-    }).removeClass("show");
-    $("#shadow").hide();
-  }
-  // 컨트롤 박스 및 배경 hide
-  function control_show() {
-    $("#control_box").css({
-      height:150
-    }).addClass("show");
-    $("#shadow").show();
-  }
-
+$(document).ready(function () {
   // 컨트롤 박스 버튼 및 배경 클릭 했을때 효과
-  $("#btn_control,#shadow").click(function(){
-    if($("#control_box").hasClass("show") == false){
+  // 이렇게 생긴 click 펑션같이 $('selector').함수 형태는 해당 selector가 생긴 후에 정의해야해서 document.ready로 꼭 들어가야함 -> document.ready는 html element랑 script 다 불러온 후에 해당 부분을 실행하겠다
+  $('#btn_control,#shadow').click(function () {
+    if ($('#control_box').hasClass('show') == false) {
       control_show();
-    } else if($("#control_box").hasClass("show") == true){
+    } else if ($('#control_box').hasClass('show') == true) {
       control_hide();
     }
   });
 
   // 예방 방법 page
   // 관련 기관 연락처 초기 상태 : show
-  $(".list_item:last-child .arrow").css({
-    transform: "translateY(-50%) rotateZ(-180deg)"
-  })
-  
+  $('.list_item:last-child .arrow').css({
+    transform: 'translateY(-50%) rotateZ(-180deg)',
+  });
+
   // 선택한 list show & hide
-  $("#prevention_list > li").each(function(index){
-    $(this).attr('data-index',index);
-  }).click(function(){
-    // 선택한 li indext 번호
-    var i = $(this).attr('data-index');
-    // 선택한 li의 보여줄 자식요소
-    var target = $(".list_item[data-index="+i+"] > :last-child");
-    // 선택한 li 화살표 요소
-    var target_arrow = $(".list_item[data-index="+i+"] .arrow");
-    
-    if(target.hasClass('show') ==false){
-      // show
-      target.slideDown();
-      target_arrow.css({
-        transform: "translateY(-50%) rotateZ(-180deg)"
-      })
-      target.addClass('show');
-    } else if( target.hasClass('show') ==true){
-      // hide
-      target.slideUp();
-      target.removeClass('show');
-      target_arrow.css({
-        transform: "translateY(-50%)"
-      })
-    }
-  })
-})
+  $('#prevention_list > li')
+    .each(function (index) {
+      $(this).attr('data-index', index);
+    })
+    .click(function () {
+      // 선택한 li indext 번호
+      var i = $(this).attr('data-index');
+      // 선택한 li의 보여줄 자식요소
+      var target = $('.list_item[data-index=' + i + '] > :last-child');
+      // 선택한 li 화살표 요소
+      var target_arrow = $('.list_item[data-index=' + i + '] .arrow');
+
+      if (target.hasClass('show') == false) {
+        // show
+        target.slideDown();
+        target_arrow.css({
+          transform: 'translateY(-50%) rotateZ(-180deg)',
+        });
+        target.addClass('show');
+      } else if (target.hasClass('show') == true) {
+        // hide
+        target.slideUp();
+        target.removeClass('show');
+        target_arrow.css({
+          transform: 'translateY(-50%)',
+        });
+      }
+    });
+});
