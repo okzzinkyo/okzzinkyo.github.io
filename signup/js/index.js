afb79b494fc2ba97aeb1834227ce968e43f1cf38
okzzinkyo <dhrwlsrud94@naver.com>
2022-01-14 17:47:23 +0900

    허용 값 이외의 text 입력 방지, 유효성 test 값 함수로 선언, 화면 zoom 방지

--- signup/js/index.js
+++ signup/js/index.js
@@ -5,13 +5,12 @@ var is_domain = 0;
 var is_SSN = 0;
 var is_asset = 0;
 
-// if (isName(value)) {
-//   is_name = 1;
-// }
-
-// if (isPhone(value)) {
-  
-// }
+var namemsg = "※ 이름을 올바르게 입력해주세요."
+var phonemsg = "※ 010,011,016,017,018,019 만 가능합니다."
+var emailmsg = "※ 영문,숫자,특수문자(.-_)포함 8~15자로 입력해주세요."
+var domainmsg = "※ (예:vinnsmail.com)와 같이 올바른 도메인으로 입력해주세요."
+var SSNfront = "※ 생년월일 6자리 입력해주세요"
+var SSNback = "※ 주민등록번호 뒤 7자리 입력해주세요"
 
 //모음, 자음을 제외한 한글 정규식
 function isName(value) {
@@ -27,7 +26,7 @@ function isPhone(value) {
 
 //이메일 ID 정규식
 function isEmailID(value) {
-  var regID = /^([a-zA-Z0-9_.-])+$/;
+  var regID = /^([\w\.\_\-])*[a-zA-Z0-9]+([\w\.\_\-])*([a-zA-Z0-9])+([\w\.\_\-])$/;
   return regID.test(value);
 }
 
@@ -39,12 +38,12 @@ function isDomain(value) {
 
 //주민등록 번호 정규식
 function isSSN(value) {
-  var regssn = /^[0-9]{6}$/
+  var regssn = /^[0-9][0-9][01][0-9][0-3][0-9]$/
    return regssn.test(value)
 }
 
 function isSSNback(value) {
-  var regssn = /^[0-9]{7}$/
+  var regssn = /^([1-4]\d{6})$/
    return regssn.test(value)
 }
 
@@ -81,6 +80,53 @@ function comma(price) {
     return price.toString().replace(/,/gi, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+function next(inputID,nextInput) {
+  $("label[for='" + inputID + "']").show();    
+  nextInput.show();    
+  nextInput.focus();
+}
+
+function pass(labelID,index) {        
+  $("input[index='" + index + "']").removeClass("error");
+  $("label[for='" + labelID + "']").removeClass("error");    
+  $("#" + labelID + "_error").html("");
+}
+
+function error(index,labelID,text) {
+  $("input[index='" + index + "']").addClass("error");
+  $("label[for='" + labelID + "']").addClass("error");
+  $("#" + labelID + "_error").html(text);
+}
+
+// 허용 외 키보드 입력 방지
+function chk_input_filter(type, obj){
+
+		var str = $(obj).val();
+
+		if(type == 'alphabet'){
+			//영문만 허용
+			$(obj).val(str.replace(/[^a-z]/gi,""));
+		}else if(type == 'number'){
+			//숫자만 허용
+			$(obj).val(str.replace(/[^0-9]/gi,""));
+		}else if(type == 'email'){
+			//이메일 숫자, 영어 특수문자(.-_)허용
+			$(obj).val(str.replace(/[^a-zA-Z0-9.-_]/gi,""));
+		}else if(type == 'non_spec'){
+			//특수문자 비허용
+			$(obj).val(str.replace(/[~!@#$%^&*()_+|<>?:;{}`\-\=\\\,.'"\[\]/]/gi,""));
+		}else if(type == 'name'){
+			//이름 한글만 허용
+      $(obj).val(str.replace(/[~!@#$%^&*()_+|<>?:;{}`\-\=\\\,.'"\[\]/a-zA-Z0-9]/gi, ""));
+    }
+}
+
+function maxLengthCheck(object){
+    if (object.value.length > object.maxLength){
+      object.value = object.value.slice(0, object.maxLength);
+    }    
+}
+  
 
 
 $(document).ready(function () {
@@ -92,17 +138,42 @@ $(document).ready(function () {
   // var inputID = $("input[index='" + index + "']").attr("id");
   $("label:not(label[for='name'])").hide();
 
-  //keyup시 자동 하이픈 생성
+  //input 자동완성 기능 off
+  $("input").attr("autocomplete", "off");
+  
+  //핸드폰 번호 keyup시 자동 하이픈 생성
   $("#phone").on("keyup",function (e) {
     e = e || window.e;
     var data = $(this).val().trim();
     this.value = autoHypen(data);
   });
 
+  // 허용된 다른 text 입력 방지
+  $("#name").on("keyup", function () {
+      chk_input_filter("name", "#name");
+  });
+  $("#email").on("keyup", function () {
+      chk_input_filter("email", "#email");
+  });
+  $("#email_Domain").on("keyup", function () {
+      chk_input_filter("email", "#email_Domain");
+  });
+  $("#SSN").on("keyup", function () {
+      chk_input_filter("number", "#SSN");
+  });
+  $("#SSN_back").on("keyup", function () {
+      chk_input_filter("number", "#SSN_back");
+  });
+
+  $("#asset").on("keyup", function () {
+      chk_input_filter("number", "#asset");
+  });
+
+  //이름 input 유효성 test
   $("#name").keypress(function (e) { 
     var Namevalue = $("#name").val();
     if (e.keyCode == 13) {
-      console.log("엔터성공");
+
       // 입련한 input의 index값
       var index = $(this).attr("index");
       //문자열 숫자로 반환
@@ -111,27 +182,27 @@ $(document).ready(function () {
       var nextInput = $("input[index='" + (index + 1) + "']");
       var labelID = $("input[index='" + index + "']").attr("id");
       var inputID = $("input[index='" + (index + 1) + "']").attr("id");
-      
-      // var nextID = $("input[index='" + (index+1) + "']").attr("id");
-      
-      console.log(isName(Namevalue))
-      if (isName(Namevalue)) {
-        $("label[for='" + inputID + "']").show();
-        nextInput.show();
-        nextInput.focus();
 
-        $("input[index='"+index+"']").removeClass("error");
-        $("label[for='"+labelID+"']").removeClass("error");
-        $("#name_error").html("");
+      if (isName(Namevalue)) {
+        next(inputID, nextInput) 
+        pass(labelID, index);
       } else {
-        is_name = 0;
-        $("input[index='"+index+"']").addClass("error");
-        $("label[for='"+labelID+"']").addClass("error");
-        $("#name_error").html("※ 이름을 올바르게 입력해주세요.");
+        error(index,labelID,namemsg)
       }
     }
   });
 
+  //이름 input value null 일때 에러메세지 제거
+  $("#name").on("propertychange change keyup paste input", function () {
+    var Namevalue = $("#name").val();
+    var index = $(this).attr("index");
+    var labelID = $("input[index='" + index + "']").attr("id");
+        if (Namevalue == "" || Namevalue == null) {
+      pass(labelID,index);
+    }
+  })
+
+  //이름 input foucus out 시 유효성 test
   $("#name").focusout(function () {
     var Namevalue = $("#name").val();
     // 입련한 input의 index값
@@ -144,25 +215,16 @@ $(document).ready(function () {
     var inputID = $("input[index='" + (index + 1) + "']").attr("id");
     
     if (isName(Namevalue)) {
-        $("label[for='" + inputID + "']").show();
-        nextInput.show();
-        nextInput.focus();
-
-        $("input[index='"+index+"']").removeClass("error");
-        $("label[for='"+labelID+"']").removeClass("error");
-        $("#name_error").html("");
+      next(inputID, nextInput);
+      pass(labelID, index);
       } else {
-        is_name = 0;
-        $("input[index='"+index+"']").addClass("error");
-        $("label[for='"+labelID+"']").addClass("error");
-        $("#name_error").html("※ 이름을 올바르게 입력해주세요.");
+      error(index, labelID, namemsg);
     }
-    
   })
-
 
   //Enter시 핸드폰 번호 유효성
   $("#phone").keypress(function (e) {
+    
     var Phonevalue = $("#phone").val();
     if (e.keyCode == 13) {
       // 입련한 input의 index값
@@ -173,42 +235,23 @@ $(document).ready(function () {
       var nextInput = $("input[index='" + (index + 1) + "']");
       var labelID = $("input[index='" + index + "']").attr("id");
       var inputID = $("input[index='" + (index + 1) + "']").attr("id");
-     
-      
+
       console.log(isPhone(Phonevalue))
       if (isPhone(Phonevalue)) {
+        $("span." + inputID + "").show();
+        $("input[index='" + (index + 2) + "']").show();
+        next(inputID, nextInput) 
+        pass(labelID, index);
         
-        $("label[for='" + inputID + "']").css({
-          display: "inline-block"
-        });
-        $("span."+inputID+"").css({
-          display: "inline-block"
-        });
-        $("input[index='" + (index + 2) + "']").css({
-          display: "inline-block"
-        });
-        
-        nextInput.css({
-          display: "inline-block"
-        });;
-        nextInput.focus();
-
-        $("input[index='" + index + "']").removeClass("error");
-        $("label[for='"+labelID+"']").removeClass("error");
-        $("#phone_error").html("");
       } else {
-        is_phone = 0;
-                
-        $("input[index='" + index + "']").addClass("error");
-        $("label[for='"+labelID+"']").addClass("error");
-        $("#phone_error").html("※010,011,016,017,018,019만 가능합니다.");
+        error(index, labelID,phonemsg);
       }
     }
   })
  
   //Enter시 이메일 ID 유효성
-  $("#email_ID").keypress(function (e) {
-    var IDvalue = $("#email_ID").val();
+  $("#email").keypress(function (e) {
+    var IDvalue = $("#email").val();
     if (e.keyCode == 13) {
       // 입련한 input의 index값
       var index = $(this).attr("index");
@@ -216,22 +259,21 @@ $(document).ready(function () {
       index = Number(index);
       //다음 input 찾기
       var nextInput = $("input[index='" + (index + 1) + "']");
-      var labelID = $("input[index='" + index + "']").attr("id");
+      var labelID = "email";
      
       console.log(isEmailID(IDvalue))
       if (isEmailID(IDvalue)) {
         nextInput.focus();
-        $("input[index='" + index + "']").removeClass("error");
-        $("label[for='"+labelID+"']").removeClass("error");
-        $("#email_error").html("");
+        pass(labelID, index);
+
       } else {
         is_emailID = 0;
-        $("input[index='" + index + "']").addClass("error");
-        $("label[for='"+labelID+"']").addClass("error");
-        $("#email_error").html("※ 영문,숫자,특수문자(.-_)포함 8~15자로 입력해주세요.");
+        error(index, labelID,emailmsg);
       }
     }
   });
+
+
 
   //Enter시 이메일 Domain 유효성
   $("#email_Domain").keypress(function (e) {
@@ -243,41 +285,27 @@ $(document).ready(function () {
       index = Number(index);
       //다음 input 찾기
       var nextInput = $("input[index='" + (index + 1) + "']");
-      var labelID = $("input[index='" + (index - 1) + "']").attr("id");
       var inputID = $("input[index='" + (index + 1) + "']").attr("id");
+      var labelID = "email";
      
-      
+      console.log(labelID);
+
       console.log(isDomain(DOMAINvalue))
       if (isDomain(DOMAINvalue)) {
-        $("label[for='" + inputID + "']").css({
-          display: "inline-block"
-        });
-        $("span."+inputID+"").css({
-          display: "inline-block"
-        });
-        $("input[index='" + (index + 2) + "']").css({
-          display: "inline-block"
-        });
-        
-        nextInput.css({
-          display: "inline-block"
-        });;
+        $("span." + inputID + "").show();
+        $("input[index='" + (index + 2) + "']").show();
+        next(inputID, nextInput) 
+        pass(labelID, index);
 
-        nextInput.focus();
-        $("input[index='" + index + "']").removeClass("error");
-        $("label[for='"+labelID +"']").removeClass("error");
-        $("#email_error").html("");
       } else {
-        $("input[index='" + index + "']").addClass("error");
-        $("label[for='"+labelID +"']").addClass("error");
-        $("#email_error").html("※ (예:vinnsmail.com)와 같이 올바른 도메인으로 입력해주세요.");
+        error(index, labelID, domainmsg );
       }
     }
   });
 
   //Enter시 주민등록번호 유효성 - 앞
-  $("#SSN_front").on("keyup",function () {
-    var frontvalue = $("#SSN_front").val();
+  $("#SSN").on("keyup",function () {
+    var frontvalue = $("#SSN").val();
     if (frontvalue.length >= 6) {
       // 입련한 input의 index값
       var index = $(this).attr("index");
@@ -285,27 +313,30 @@ $(document).ready(function () {
       index = Number(index);
       //다음 input 찾기
       var nextInput = $("input[index='" + (index + 1) + "']");
-      var inputID = $("input[index='" + index + "']").attr("id");
-     
-      
+      var labelID = "SSN";
+
       console.log(isSSN(frontvalue))
       if (isSSN(frontvalue)) {
-        $("input[index='" + index + "']").removeClass("error");
-        $("label[for='"+inputID+"']").removeClass("error");
         $("#SSN_back").removeAttr("disabled");
-        nextInput.focus();
-        $("#SSN_error").html("");
+        nextInput.focus();  
+        pass(labelID, index);
+
       } else {
-        $("input[index='" + index + "']").addClass("error");
-        $("label[for='"+inputID+"']").addClass("error");
-        $("#SSN_error").html("※ 생년월일 6자리 입력해주세요");
+        error(index, labelID, SSNfront);
+        
       }
+      
     }
+
+  //     if ($("#SSN").val().length >= 6) {
+  //   console.log($("#SSN").val().str(0, 6));
+  //   // $("#SSN").val($("#SSN").str(0, 6));
+  // }
   });
 
-  $("#SSN_front").change(function () {
+  // $("#SSN_front").change(function () {
     
-  });
+  // });
 
 
 
@@ -320,23 +351,18 @@ $(document).ready(function () {
       //다음 input 찾기
       var nextInput = $("input[index='" + (index + 1) + "']");
       var inputID = $("input[index='" + (index+1) + "']").attr("id");
-      var lebelID = $("input[index='" + (index-1) + "']").attr("id");
+      var labelID = "SSN"
      
       console.log(isSSNback(backvalue))
       if (isSSNback(backvalue)) {
-        $("label[for='" + inputID + "']").show();
-        $("p." + inputID + "> span").css({
-          display: "inline-block"
-        });
-        nextInput.show();
-        nextInput.focus();
-        $("input[index='" + index + "']").removeClass("error");
-        $("label[for='"+lebelID+"']").removeClass("error");
-        $("#SSN_error").html("");
+        $("p." + inputID + "").show();
+        $("input[index='" + (index + 2) + "']").show();
+        next(inputID, nextInput) 
+        pass(labelID, index);
+
       } else {
-        $("input[index='" + index + "']").addClass("error");
-        $("label[for='"+lebelID+"']").addClass("error");
-        $("#SSN_error").html("※ 주민등록번호 뒤 7자리 입력해주세요");
+        error(index, labelID, SSNback);
+        
       }
     }
   });
@@ -383,11 +409,33 @@ $(document).ready(function () {
     }
   });
 
-  $("#btn_join").click(function () {
-    alert("회원가입 성공");
-  })
+  // $("#btn_join").click(function () {
+  //   alert("회원가입 성공");
+  // })
   
+  
+  $("#btn_join").click(function () {
+    var Namevalue = $("#name").val();
+    // 입련한 input의 index값
+    var index = $("#name").attr("index");
+    //문자열 숫자로 반환
+    index = Number(index);
+    //다음 input 찾기
+    var labelID = $("input[index='" + index + "']").attr("id");("id");
+    
+    if (isName(Namevalue)) {
+      is_name = 1;
+      pass(labelID, index);
+    } else {
+      is_name = 0;
+      error(index, labelID, namemsg);
+    }
 
+    if (is_name == 1) {
+      alert("회원가입 성공");
+      $("#btn_join").submit();
+    }
+  })
 
   
     
